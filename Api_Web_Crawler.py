0c7ac4d2118b7d32f002522d658672d12d1fe46e
Kittipat Dechkul <unk.atreus@gmail.com>
2020-02-08 15:40:11 +0700

    indent

--- Api_Web_Crawler.py
+++ Api_Web_Crawler.py
@@ -7,10 +7,10 @@ url = "https://theinternship.io/"
 test = []
 
 for line in Logo_url:
-    test.append(url+line)
+    test.append(url + line)
 
 score_titles = {"companies": [{"logo": t} for t in test]}
 
 
 if __name__ == '__main__':
-    print (json.dumps(score_titles,indent=2))
+    print (json.dumps(score_titles, indent=2))
